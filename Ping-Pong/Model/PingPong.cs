3d26ec447f2999e01bffebb5862f3574c96a9b5d
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-02-12 19:41:36 -0800

    add method Main() and build executable file

--- Ping-Pong/Model/PingPong.cs
+++ Ping-Pong/Model/PingPong.cs
@@ -2,5 +2,35 @@ using System;
 
 namespace Game
 {
-  class PingPong
+  public class PingPong
+  {
+    public static string Convert(int input)
+    {
+      string output = "";
+      if (input % 3 == 0)
+      {
+        output = "ping ";
+      }
+      if (input % 5 == 0)
+      {
+        output += "pong ";
+      }
+      if (output == "")
+      {
+        output = input.ToString();
+      }
+      return output.Trim();
+    }
+    public static void Main()
+    {
+      Console.WriteLine("Enter a number:");
+      int inputNumber = int.Parse(Console.ReadLine());
+
+      for (int i = 1; i <= inputNumber; i++)
+      {
+          Console.WriteLine(Convert(i));
+      }
+
+    }
+  }
 }
