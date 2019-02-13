c9d8a4465676df19f4a877a32faded462ae601e6
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-02-12 18:36:33 -0800

    add initial files, add contact as list item in ul

---
+++ Ping-Pong/Model/PingPong.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace Game
+{
+  class PingPong
+}
---
+++ Ping-PongTests/ModelTests/PingPongTest.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PingPong;
+
+namespace Game.Tests
+{
+  [TestClass]
+  public class PingPongTest
+  {
+    [TestMethod]
+    // Test method will go here
+  }
+}
